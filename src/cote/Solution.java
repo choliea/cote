524756b67d2363b19201fdc3d8f92902ae5e70c7
yukang <choliea91@gmail.com>
2023-09-13 01:40:36 +0900

    d

--- src/cote/Solution.java
+++ src/cote/Solution.java
@@ -2,20 +2,10 @@ package cote;
 
 public class Solution {
 
-	public int solution(String[] spell, String[] dic) {
-		int answer = 0;
-		for (int i = 0; i < dic.length; i++) {
-			boolean b = true;
-			for (int j = 0; j < spell.length; j++) {
-				if(dic[i].length()-1!=dic[i].replace(spell[j], "").length()) {
-					b=false;
-					break;
-				}
-			}
-			if(b)return 1;
-		}
-		return 2;
-	}
+	  public int solution(String my_string) {
+	        int answer = 0;
+	        return answer;
+	    }
 
 	public static void main(String[] args) {
 		String[] a = { "z", "d", "x" };
