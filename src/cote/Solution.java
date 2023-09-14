8b7270bf35405b62175ac750d85467f4a189d3ac
yukang <choliea91@gmail.com>
2023-09-14 20:37:41 +0900

    1

--- src/cote/Solution.java
+++ src/cote/Solution.java
@@ -5,26 +5,73 @@ import java.util.Queue;
 
 public class Solution {
 
-    public int solution(int[] numbers, int k) {
-        Queue<Integer> list = new LinkedList<>();
-        for (int j = 0; j < numbers.length; j++) {
-        	list.offer(numbers[j]);
-		}
-        for (int i = 0; i < k-1; i++) {
-			list.offer(list.poll());
-			list.offer(list.poll());
-		}
-        return list.peek();
+	public long solution(String numbers) {
+        long answer = 0; 
+        int len = numbers.length();
+        while(numbers.length()>2){
+         
+        if(numbers.startsWith("zero")){
+            answer= answer*10;
+           numbers= numbers.replaceFirst("zero","");
+        }else if(numbers.startsWith("one")){
+            answer= answer*10+1;
+            numbers=numbers.replaceFirst("one","");
+        }else if(numbers.startsWith("two")){
+            answer= answer*10+2;
+            numbers=numbers.replaceFirst("two","");
+        }else if(numbers.startsWith("three")){
+            answer= answer*10+3;
+            numbers=numbers.replaceFirst("three","");
+        }else if(numbers.startsWith("four")){
+            answer= answer*10+4;
+            numbers= numbers.replaceFirst("four","");
+        }else if(numbers.startsWith("five")){
+            answer= answer*10+5;
+            numbers= numbers.replaceFirst("five","");
+        }else if(numbers.startsWith("six")){
+            answer= answer*10+6;
+            numbers= numbers.replaceFirst("six","");
+        }else if(numbers.startsWith("seven")){
+            answer= answer*10+7;
+            numbers=  numbers.replaceFirst("seven","");
+        }else if(numbers.startsWith("eight")){
+            answer= answer*10+8;
+            numbers= numbers.replaceFirst("eight","");
+        }else if(numbers.startsWith("nine")){
+            answer= answer*10+9;
+            numbers= numbers.replaceFirst("nine","");
+        }
+            if(numbers.length()<3)break;
+            }
+        return answer;
     }
-    public int solution2(int[] numbers, int k) {
-    	
-    	return numbers[(2*k-2)%numbers.length];
+public long solution3(String numbers) {
+	String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    for (int i = 0; i < numbers_arr.length; i++) {
+        numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
     }
-	
-	
+
+    long answer = Long.parseLong(numbers);
+    return answer;
+}
+public long solution2(String numbers) {
+	 return Long.parseLong(
+             numbers.replaceAll("zero", "0")
+                     .replaceAll("one", "1")
+                     .replaceAll("two", "2")
+                     .replaceAll("three", "3")
+                     .replaceAll("four", "4")
+                     .replaceAll("five", "5")
+                     .replaceAll("six", "6")
+                     .replaceAll("seven", "7")
+                     .replaceAll("eight", "8")
+                     .replaceAll("nine", "9")
+     );
+}
 	public static void main(String[] args) {
 		int[] a = { 1, 2, 3, 4, 5, 6 };
 		String[] d = { "def", "dww", "dzx", "loveaw" };
-		System.out.println(new Solution().solution2(a,5));
+		System.out.println(new Solution().solution("onetwoonefouronesixseveneightnine"));
 	}
 }
